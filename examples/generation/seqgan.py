7a0e0c03b793dd751dd4508efa1c1931b916b3e9
Masato Hagiwara <hagisan@gmail.com>
2019-04-05 12:26:24 -0400

    Pre-train generator

--- examples/generation/seqgan.py
+++ examples/generation/seqgan.py
@@ -17,14 +17,15 @@ from allennlp.modules.seq2seq_encoders import PytorchSeq2SeqWrapper
 from allennlp.modules.seq2vec_encoders import CnnEncoder
 from allennlp.modules.text_field_embedders import TextFieldEmbedder, BasicTextFieldEmbedder
 from allennlp.modules.token_embedders import Embedding
-from allennlp.nn.util import get_text_field_mask
+from allennlp.nn.util import get_text_field_mask, sequence_cross_entropy_with_logits
 from allennlp.training.trainer import Trainer
 
 EMBEDDING_SIZE = 32
 HIDDEN_SIZE = 256
 BATCH_SIZE = 256
-D_STEPS = 5
+D_STEPS = 3
 D_EPOCHS = 3
+G_EPOCHS = 10
 
 class Generator(Model):
     def __init__(self,
@@ -41,6 +42,15 @@ class Generator(Model):
         self.hidden2out = torch.nn.Linear(in_features=self.rnn.get_output_dim(),
                                           out_features=vocab.get_vocab_size('tokens'))
         self.max_len = max_len
+
+    def forward(self, input_tokens, output_tokens):
+        mask = get_text_field_mask(input_tokens)
+        embeddings = self.embedder(input_tokens)
+        rnn_hidden = self.rnn(embeddings, mask)
+        out_logits = self.hidden2out(rnn_hidden)
+        loss = sequence_cross_entropy_with_logits(out_logits, output_tokens['tokens'], mask)
+
+        return {'loss': loss}
 
     def generate(self) -> Tuple[List[Token], torch.tensor]:
 
@@ -64,7 +74,7 @@ class Generator(Model):
             output = self.hidden2out(output)
 
             dist = torch.softmax(output[0, 0], dim=0)
-            dist = .99 * dist + .01 * uniform
+            # dist = .99 * dist + .01 * uniform
 
             word_idx = start_symbol_idx
 
@@ -129,14 +139,25 @@ def read_shakespeare(all_chars: Set[str]=None) -> List[List[Token]]:
     return sentences
 
 
-def text_to_instance(tokens: List[Token],
-                     label: str,
-                     token_indexers: Dict[str, TokenIndexer]):
-
+def text_to_disc_instance(tokens: List[Token],
+                          label: str,
+                          token_indexers: Dict[str, TokenIndexer]):
     fields = {'tokens': TextField(tokens, token_indexers),
               'label': LabelField(label)}
 
     return Instance(fields)
+
+
+def text_to_ml_instance(tokens: List[Token],
+                        token_indexers: Dict[str, TokenIndexer]):
+    tokens = list(tokens)   # shallow copy
+    tokens.insert(0, Token(START_SYMBOL))
+    tokens.append(Token(END_SYMBOL))
+
+    input_field = TextField(tokens[:-1], token_indexers)
+    output_field = TextField(tokens[1:], token_indexers)
+    return Instance({'input_tokens': input_field,
+                     'output_tokens': output_field})
 
 
 def get_discriminator_batch(generator: Generator,
@@ -147,7 +168,7 @@ def get_discriminator_batch(generator: Generator,
     sent_ids = np.random.choice(len(train_set), size=BATCH_SIZE, replace=False)
     for sent_id in sent_ids:
         tokens = train_set[sent_id]
-        instance = text_to_instance(tokens, 'real', token_indexers)
+        instance = text_to_disc_instance(tokens, 'real', token_indexers)
         instances.append(instance)
 
     # Generate fake batch
@@ -157,7 +178,7 @@ def get_discriminator_batch(generator: Generator,
         if not words:
             continue
 
-        instance = text_to_instance(words, 'fake', token_indexers)
+        instance = text_to_disc_instance(words, 'fake', token_indexers)
         instances.append(instance)
         num_fake_instances += 1
 
@@ -178,7 +199,7 @@ def get_generator_batch(generator: Generator,
         while len(words) <= 4:
             words.append(Token(text=DEFAULT_PADDING_TOKEN))
 
-        instance = text_to_instance(words, 'fake', token_indexers)
+        instance = text_to_disc_instance(words, 'fake', token_indexers)
         instances.append((instance, log_likelihood))
 
         num_instances += 1
@@ -197,11 +218,11 @@ def get_reward(instance: Instance,
 
 
 def main():
-    generator_lr = 5.e-2
+    generator_lr = 1.e-3
     print('generator_lr: {}'.format(generator_lr))
 
     all_chars = {END_SYMBOL, START_SYMBOL}
-    all_chars.update("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,!?'")
+    all_chars.update("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .,!?'-")
     token_counts = {char: 1 for char in all_chars}
     label_counts = {'real': 1, 'fake': 1}
 
@@ -217,14 +238,36 @@ def main():
     generator = Generator(embedder, vocab)
     discriminator = Discriminator(embedder, vocab)
 
-    generator_optim = optim.Adam(generator.parameters())
-    discriminator_optim = optim.Adam(discriminator.parameters())
+    generator_optim = optim.Adam(generator.parameters(), lr=generator_lr)
+    discriminator_optim = optim.Adagrad(discriminator.parameters())
 
+    # pre-train generator
+    instances = [text_to_ml_instance(tokens, token_indexers)
+                 for tokens in train_set]
+    iterator = BasicIterator(batch_size=BATCH_SIZE)
+    iterator.index_with(vocab)
 
-    for epoch in range(100):
+    trainer = Trainer(model=generator,
+                      optimizer=generator_optim,
+                      iterator=iterator,
+                      train_dataset=instances,
+                      num_epochs=G_EPOCHS)
+    trainer.train()
+
+    for epoch in range(500):
         # train discriminator
+        fake_logs = []
+        real_logs = []
         for d_step in range(D_STEPS):
             instances = get_discriminator_batch(generator, train_set, token_indexers)
+            if d_step == 0:
+                for inst in instances:
+                    text = ''.join(token.text for token in inst.fields['tokens'])
+                    label = inst.fields['label'].label
+                    if label == 'real' and len(real_logs) < 10:
+                        real_logs.append('    {:70s} {}'.format(text, label))
+                    if label == 'fake' and len(fake_logs) < 10:
+                        fake_logs.append('    {:70s} {}'.format(text, label))
 
             iterator = BasicIterator(batch_size=2 * BATCH_SIZE)
             iterator.index_with(vocab)
@@ -235,6 +278,10 @@ def main():
                               train_dataset=instances,
                               num_epochs=D_EPOCHS)
             trainer.train()
+
+        print('epoch: {}, step-D'.format(epoch))
+        for log in real_logs + fake_logs:
+            print(log)
 
         # train generator
         generator.zero_grad()
